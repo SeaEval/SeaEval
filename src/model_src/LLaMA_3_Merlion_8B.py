40bced69cb8a51cb52fd9b5e1ce77f5f47e917e2
Bin Wang <bwang28c@gmail.com>
2024-05-11 12:59:06 +0300

    new

--- src/model_src/LLaMA_3_Merlion_8B.py
+++ src/model_src/LLaMA_3_Merlion_8B.py
@@ -32,21 +32,29 @@ def LLaMA_3_Merlion_8B_model_loader(self):
 
 def LLaMA_3_Merlion_8B_model_generation(self, batch_input):
 
+    '''
     batch_input_templated = []
     for sample in batch_input:    
         #messages = [{"role": "user", "content": sample}]
         #sample_templated = self.tokenizer.apply_chat_template(messages, return_tensors="pt", tokenize=False)
-        sample_templated = "[INST] " + sample + " [/INST]"
+        sample_templated = "<s>[INST] " + sample + " [/INST]"
         batch_input_templated.append(sample_templated)
 
     batch_input = batch_input_templated
+    '''
+    
+    # add template
+    batch_input = ["[INST] "+sample+" [/INST]" for sample in batch_input]
 
     encoded_batch        = self.tokenizer(batch_input, return_tensors="pt", padding=True).to(self.model.device)
     generated_ids        = self.model.generate(**encoded_batch, max_new_tokens=self.max_new_tokens, pad_token_id=self.tokenizer.eos_token_id)
     generated_ids        = generated_ids[:, encoded_batch.input_ids.shape[-1]:]
     decoded_batch_output = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 
+    # remove '</s>'
+    decoded_batch_output = [output.strip('</s>').strip() for output in decoded_batch_output]
+
     # remove anything after '\n'
-    # decoded_batch_output = [output.strip().split('\n')[0] for output in decoded_batch_output]
+    decoded_batch_output = [output.strip('Answer:').strip().split('\n')[0] for output in decoded_batch_output]
 
     return decoded_batch_output
